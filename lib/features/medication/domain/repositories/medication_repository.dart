fba9243ab615ebd06b9c6c3a1a598f0d69f74e63
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-19 12:29:29 +0700

    Add medication delete repository contract

--- lib/features/medication/domain/repositories/medication_repository.dart
+++ lib/features/medication/domain/repositories/medication_repository.dart
@@ -3,6 +3,7 @@ import '../entities/medication.dart';
 abstract interface class MedicationRepository {
   List<Medication> readAll();
   Future<void> replaceAll(List<Medication> medications);
+  Future<void> delete(String id);
 }
 
 abstract interface class DoseLogRepository {
