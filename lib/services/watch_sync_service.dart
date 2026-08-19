403b43006a6785a5fc1b116abd449b4b8707c6cc
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-19 12:57:22 +0700

    Remove persistence mapping from watch sync

--- lib/services/watch_sync_service.dart
+++ lib/services/watch_sync_service.dart
@@ -3,12 +3,26 @@ import 'package:flutter/services.dart';
 import '../models/medication.dart';
 
 class WatchSyncService {
-  static const MethodChannel _channel = MethodChannel('med_reminder/watch_sync');
+  static const MethodChannel _channel = MethodChannel(
+    'med_reminder/watch_sync',
+  );
 
   static Future<void> syncMeds(List<Medication> meds) async {
     await _channel.invokeMethod<void>(
       'syncMeds',
-      meds.map((med) => med.toMap()).toList(),
+      meds
+          .map(
+            (med) => <String, dynamic>{
+              'id': med.id,
+              'name': med.name,
+              'times': med.times,
+              'dosagePerTime': med.dosagePerTime,
+              'mode': med.mode.name,
+              'initialAmount': med.initialAmount,
+              'lowThreshold': med.lowThreshold,
+            },
+          )
+          .toList(growable: false),
     );
   }
 }
