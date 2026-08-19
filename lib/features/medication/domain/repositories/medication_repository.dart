8ea76b5db357027fe1f4207a7136b5cfdfa80b43
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-19 12:41:35 +0700

    Return Result from repository contracts

--- lib/features/medication/domain/repositories/medication_repository.dart
+++ lib/features/medication/domain/repositories/medication_repository.dart
@@ -1,12 +1,13 @@
+import '../../../../core/result/result.dart';
 import '../entities/medication.dart';
 
 abstract interface class MedicationRepository {
-  List<Medication> readAll();
-  Future<void> replaceAll(List<Medication> medications);
-  Future<void> delete(String id);
+  Result<List<Medication>> readAll();
+  Future<Result<void>> replaceAll(List<Medication> medications);
+  Future<Result<void>> delete(String id);
 }
 
 abstract interface class DoseLogRepository {
-  List<DoseLog> readAll();
-  Future<void> replaceAll(List<DoseLog> logs);
+  Result<List<DoseLog>> readAll();
+  Future<Result<void>> replaceAll(List<DoseLog> logs);
 }
