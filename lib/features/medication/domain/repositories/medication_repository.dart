01e210b1564e7f8fb899c20a98804fe2219bf031
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-21 06:30:48 +0700

    refactor: separate medication and dose log repositories

--- lib/features/medication/domain/repositories/medication_repository.dart
+++ lib/features/medication/domain/repositories/medication_repository.dart
@@ -1,13 +1,10 @@
 import '../../../../core/result/result.dart';
 import '../entities/medication.dart';
 
+export 'dose_log_repository.dart';
+
 abstract interface class MedicationRepository {
   Result<List<Medication>> readAll();
   Future<Result<void>> replaceAll(List<Medication> medications);
   Future<Result<void>> delete(String id);
-}
-
-abstract interface class DoseLogRepository {
-  Result<List<DoseLog>> readAll();
-  Future<Result<void>> replaceAll(List<DoseLog> logs);
 }
