139a9c1d350ce2b2ebd83195f2756296766b4205
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-23 17:03:21 +0700

    feat(summary): compose doctor visit summary provider

---
+++ lib/features/doctor_visit_summary/presentation/providers/doctor_visit_summary_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../appointment/presentation/providers/appointment_providers.dart';
+import '../../../medication/presentation/viewmodels/medication_view_model.dart';
+import '../../../medication_checkin/presentation/providers/medication_check_in_providers.dart';
+import '../../../refill/presentation/providers/refill_providers.dart';
+import '../../application/build_doctor_visit_summary.dart';
+import '../../domain/entities/doctor_visit_summary.dart';
+
+final doctorVisitSummaryClockProvider = Provider<DateTime Function()>(
+  (ref) => DateTime.now,
+);
+
+final doctorVisitSummaryProvider = Provider<DoctorVisitSummary>((ref) {
+  const builder = BuildDoctorVisitSummary();
+  return builder(
+    now: ref.watch(doctorVisitSummaryClockProvider)(),
+    medications: ref.watch(medsProvider),
+    doseLogs: ref.watch(logsProvider),
+    refillEvents: ref.watch(refillEventsProvider),
+    checkIns: ref.watch(medicationCheckInsProvider),
+    appointments: ref.watch(appointmentsProvider),
+  );
+});
