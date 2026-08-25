fbf59c747911a5c0d668cb581642d475b48730a9
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-25 08:11:24 +0700

    Make backup restore post-success repair explicit

--- lib/features/backup/application/restore_backup_bundle.dart
+++ lib/features/backup/application/restore_backup_bundle.dart
@@ -4,7 +4,7 @@ import '../../../core/result/result.dart';
 import 'commit_prepared_backup_restore.dart';
 import 'prepare_backup_restore.dart';
 
-typedef BackupRestoreSuccessCallback = void Function();
+typedef BackupRestoreSuccessCallback = Future<Result<void>> Function();
 
 final class RestoreBackupBundle {
   const RestoreBackupBundle({
@@ -27,7 +27,10 @@ final class RestoreBackupBundle {
         await commit((prepared as Success<PreparedBackupRestore>).value);
     if (committed case Failed<void>()) return committed;
 
-    onSuccess?.call();
+    final successCallback = onSuccess;
+    if (successCallback != null) {
+      return successCallback();
+    }
     return const Success<void>(null);
   }
 }
