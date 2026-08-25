5747fa5a618deb8cdd2a1d0e625cba18d00e006c
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-25 22:45:06 +0700

    fix: allow coordinator without permission snapshot reader

--- lib/features/medication/application/reminder_system_trigger_coordinator.dart
+++ lib/features/medication/application/reminder_system_trigger_coordinator.dart
@@ -1,34 +1,34 @@
 final class ReminderSystemTriggerCoordinator {
   const ReminderSystemTriggerCoordinator({
     required this.refreshTimezoneIfChanged,
-    required this.refreshPermissionStateIfChanged,
+    this.refreshPermissionStateIfChanged,
     required this.requestNotificationPermission,
     required this.requestExactAlarmPermission,
     required this.reconcile,
   });
 
   final Future<bool> Function() refreshTimezoneIfChanged;
-  final Future<bool> Function() refreshPermissionStateIfChanged;
+  final Future<bool> Function()? refreshPermissionStateIfChanged;
   final Future<bool> Function() requestNotificationPermission;
   final Future<bool> Function() requestExactAlarmPermission;
   final Future<void> Function() reconcile;
 
   Future<void> onResume() async {
     await refreshTimezoneIfChanged();
-    await refreshPermissionStateIfChanged();
+    await refreshPermissionStateIfChanged?.call();
     await reconcile();
   }
 
   Future<bool> requestNotifications() async {
     final granted = await requestNotificationPermission();
-    await refreshPermissionStateIfChanged();
+    await refreshPermissionStateIfChanged?.call();
     await reconcile();
     return granted;
   }
 
   Future<bool> requestExactAlarm() async {
     final granted = await requestExactAlarmPermission();
-    await refreshPermissionStateIfChanged();
+    await refreshPermissionStateIfChanged?.call();
     await reconcile();
     return granted;
   }
