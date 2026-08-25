f87bb4649158387f20904ae558dc892f78546ebe
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-25 12:08:18 +0700

    Add backup import selection port

---
+++ lib/features/backup/application/backup_import_port.dart
@@ -0,0 +1,15 @@
+import 'dart:typed_data';
+
+import '../../../core/result/result.dart';
+
+final class BackupImportSelection {
+  BackupImportSelection({required this.fileName, required Uint8List bytes})
+      : bytes = Uint8List.fromList(bytes);
+
+  final String fileName;
+  final Uint8List bytes;
+}
+
+abstract interface class BackupImportPort {
+  Future<Result<BackupImportSelection?>> pickArchive();
+}
