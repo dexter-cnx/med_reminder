82c2b2d7b944147356f95ec81e2ee9408bac32c3
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-25 22:37:10 +0700

    Route foreground permission state through reminder reconciliation

--- lib/features/medication/application/reminder_system_trigger_coordinator.dart
+++ lib/features/medication/application/reminder_system_trigger_coordinator.dart
@@ -1,29 +1,34 @@
 final class ReminderSystemTriggerCoordinator {
   const ReminderSystemTriggerCoordinator({
     required this.refreshTimezoneIfChanged,
+    required this.refreshPermissionStateIfChanged,
     required this.requestNotificationPermission,
     required this.requestExactAlarmPermission,
     required this.reconcile,
   });
 
   final Future<bool> Function() refreshTimezoneIfChanged;
+  final Future<bool> Function() refreshPermissionStateIfChanged;
   final Future<bool> Function() requestNotificationPermission;
   final Future<bool> Function() requestExactAlarmPermission;
   final Future<void> Function() reconcile;
 
   Future<void> onResume() async {
     await refreshTimezoneIfChanged();
+    await refreshPermissionStateIfChanged();
     await reconcile();
   }
 
   Future<bool> requestNotifications() async {
     final granted = await requestNotificationPermission();
+    await refreshPermissionStateIfChanged();
     await reconcile();
     return granted;
   }
 
   Future<bool> requestExactAlarm() async {
     final granted = await requestExactAlarmPermission();
+    await refreshPermissionStateIfChanged();
     await reconcile();
     return granted;
   }
